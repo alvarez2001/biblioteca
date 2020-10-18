44d3d17286389c9cfe68a8ca9fefd71a0c4bd9e2
Jose Miguel <66285893+alvarez2001@users.noreply.github.com>
2020-10-18 18:58:48 -0400

    Update global.ts

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,3 @@
 export var Global = {
-    /* url : 'http://192.168.1.14/biblioteca/public/api/' */
-    /* url : 'http://192.168.43.116/biblioteca/public/api/' */
-    url:'https://injeas.com/apiBiblioteca/public/api/'
+     url : 'http://192.168.43.116/biblioteca/public/api/' 
 }
